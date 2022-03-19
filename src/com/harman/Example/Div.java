f30e497ed36a9ac52669c958a45c6bfa06f6fd15
3dhanushkrishna <3dhanushkccd@gmail.com>
2022-03-19 23:33:00 +0530

    division method

---
+++ src/com/harman/Example/Div.java
@@ -0,0 +1,12 @@
+package com.harman.Example;
+
+public class Div {
+    public static void main(String[] args) {
+        int x=16;
+        int y=9;
+        int z=x/y;
+        System.out.println(z);
+    }
+}
+
+
