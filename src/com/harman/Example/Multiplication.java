8ea14428612950b174424ecc0b7b7f11f3e3b1de
likhithar2507 <likhithagowda2507@gmail.com>
2022-03-19 23:22:12 +0530

    mul

---
+++ src/com/harman/Example/Multiplication.java
@@ -0,0 +1,10 @@
+package com.harman.Example;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int x=9;
+        int y= 3;
+        int z=x*y;
+        System.out.println(z);
+    }
+}
