4bde949a453bf893d4df86fae973cef6f51d1370
likhithar2507 <likhithagowda2507@gmail.com>
2022-03-19 23:11:39 +0530

    add

---
+++ src/com/harman/Example/Add.java
@@ -0,0 +1,10 @@
+package com.harman.Example;
+
+public class Add {
+    public static void main(String[] args) {
+        int x=8;
+        int y=9;
+        int z=x+y;
+        System.out.println(z);
+    }
+}
