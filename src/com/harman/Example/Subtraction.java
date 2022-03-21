a0347d1a6347a42cb11f342dea485dad24d8bcb5
Pradeepak5 <pradeepak571@gmail.com>
2022-03-21 08:30:21 +0530

    sub

---
+++ src/com/harman/Example/Subtraction.java
@@ -0,0 +1,13 @@
+package com.harman.Example;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		int x=9;
+        int y= 3;
+        int z=x-y;
+        System.out.println(z);
+
+	}
+
+}
